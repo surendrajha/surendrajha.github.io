5c96114a176dfd0bff44aee9800162b53a90a8a5
Surendra Jha <surendra.jha@outlook.com>
2017-03-19 17:47:26 +0530

    added capcha fixes

--- contact_me.php
+++ contact_me.php
@@ -10,55 +10,68 @@ $htmlHeader = '';
 $htmlFooter = '';
 $okMessage = 'Contact form succesfully submitted. Thank you, I will get back to you soon!';
 $htmlContent = '<h1>New message from contact form</h1>';
+print_r("dasjkhasjdh");
+$secret = "6LffjRkUAAAAAEJq_uzeSCbcrizwAx5WZ9Ww2GcH";
+$response = null;
+$reCaptcha = new ReCaptcha($secret);
 
-/* DO NOT EDIT BELOW */
-/* use classes */
+print_r($reCaptcha);
+die();
 
-use Nette\Mail\Message,
-    Nette\Mail\SendmailMailer;
-
-/* require framework */
-require 'data/Nette/nette.phar';
-
-/* configure neccessary */
-$configurator = new Nette\Configurator;
-$configurator->setTempDirectory(__DIR__ . '/data/temp');
-$container = $configurator->createContainer();
-
-/* get post */
-$httpRequest = $container->getService('httpRequest');
-$httpResponse = $container->getService('httpResponse');
-
-$post = $httpRequest->getPost();
-if ($httpRequest->isAjax()) {
-    /* compose htmlContent */
-
-    $htmlContent .= '<table>';
-    foreach ($post as $key => $value) {
-
-	if (isset($fields[$key])) {
-	    $htmlContent .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
-	}
-    }
-    $htmlContent .= '</table>';
-
-    /* compose html body */
-    $htmlBody = $htmlHeader . $htmlContent . $htmlFooter;
-
-    /* send email */
-    $mail = new Message;
-    $mail->setFrom($from)
-	    ->addTo($sendTo)
-	    ->setSubject($subject)
-	    ->setHtmlBody($htmlBody, FALSE);
-
-    $mailer = new SendmailMailer;
-    $mailer->send($mail);
-
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-
-    $httpResponse->setCode(200);
-    $response = new \Nette\Application\Responses\JsonResponse($responseArray);
-    $response->send($httpRequest, $httpResponse);
+if ($_POST["g-recaptcha-response"]) {
+    $response = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
 }
 
+
+if ($response != null && $response->success) {
+    /* DO NOT EDIT BELOW */
+    /* use classes */
+    use Nette\Mail\Message,
+        Nette\Mail\SendmailMailer;
+
+    /* require framework */
+    require 'data/Nette/nette.phar';
+
+    /* configure neccessary */
+    $configurator = new Nette\Configurator;
+    $configurator->setTempDirectory(__DIR__ . '/data/temp');
+    $container = $configurator->createContainer();
+
+    /* get post */
+    $httpRequest = $container->getService('httpRequest');
+    $httpResponse = $container->getService('httpResponse');
+
+    $post = $httpRequest->getPost();
+    if ($httpRequest->isAjax()) {
+        /* compose htmlContent */
+        $htmlContent .= '<table>';
+        foreach ($post as $key => $value) {
+    	   if (isset($fields[$key])) {
+    	       $htmlContent .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+    	   }
+        }
+        $htmlContent .= '</table>';
+
+        /* compose html body */
+        $htmlBody = $htmlHeader . $htmlContent . $htmlFooter;
+
+        /* send email */
+        $mail = new Message;
+        $mail->setFrom($from)
+    	    ->addTo($sendTo)
+    	    ->setSubject($subject)
+    	    ->setHtmlBody($htmlBody, FALSE);
+
+        $mailer = new SendmailMailer;
+        $mailer->send($mail);
+
+        $responseArray = array('type' => 'success', 'message' => $okMessage);
+
+        $httpResponse->setCode(200);
+        $response = new \Nette\Application\Responses\JsonResponse($responseArray);
+        $response->send($httpRequest, $httpResponse);
+    }
+}
